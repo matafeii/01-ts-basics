9695e792c218f56b7f7ac5478114021978d418ae
Kit_Kat <matafeii38@gmail.com>
2026-04-14 11:13:42 +0300

    finish_task_7_package_lock_package

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,4 +1,19 @@
-// Task 8
-// Add the solution for the eighth task in this file.
+import axios from 'axios';
 
-export {};
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    'https://jsonplaceholder.typicode.com/posts',
+  );
+
+  return response.data;
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
